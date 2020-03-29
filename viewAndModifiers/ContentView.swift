6be3ddc19a7d0d88c799947c2f8576808f177d7f
MarkStudio <markchen8024@gmail.com>
2020-03-29 21:11:27 +0800

    adding custom modifier

--- viewAndModifiers/ContentView.swift
+++ viewAndModifiers/ContentView.swift
@@ -8,42 +8,22 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0 ..< rows, id: \.self) { row in
-                HStack {
-                    ForEach(0 ..< self.columns, id: \.self) { column in
-                        self.content(row, column)
-                    }
-                }
-            }
-        }
-    }
-    
-    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
-        self.rows = rows
-        self.columns = columns
-        self.content = content
+struct PrimaryLabel: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .font(.largeTitle)
     }
 }
-
 
 struct ContentView: View {
     
     
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col  in
-//            HStack {
-                Image(systemName: "\(row * 4 + col).circle")
-                Text("R\(row) C\(col)")
-//            }
-           
-        }
+        Text("Hello World")
+        .modifier(PrimaryLabel())
       
     }
 }
